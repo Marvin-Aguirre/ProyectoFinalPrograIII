bc3a33a12de47c3135d364c9bd579c654c58df1c
Marvin <aguirreestuardo758@gmail.com>
2022-05-29 18:18:58 -0600

    first commit

---
+++ src/main/java/com/example/demo/ProyectoFinalPrograIiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPrograIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPrograIiiApplication.class, args);
+	}
+
+}
